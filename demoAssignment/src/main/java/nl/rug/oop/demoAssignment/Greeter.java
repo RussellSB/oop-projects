fbab0dd3616d05aff1e41c4a45659992e4ddec42
Max Verbeek <m4xv3rb33k@gmail.com>
2019-04-16 13:53:41 +0200

    Add a demo assignment

---
+++ demoAssignment/src/main/java/nl/rug/oop/demoAssignment/Greeter.java
@@ -0,0 +1,34 @@
+package nl.rug.oop.demoAssignment;
+
+/**
+ * Create nice hello and goodbye messages for a particular person.
+ */
+public class Greeter {
+
+    private String name;
+
+    /**
+     * Constructor
+     *
+     * @param name  The name that is greeted
+     */
+    public Greeter(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Creates a "hello" greeting
+     * @return a nice hello message.
+     */
+    public String sayHello() {
+        return "";
+    }
+
+    /**
+     * Creates a "goodbye" message
+     * @return a nice goodbye message.
+     */
+    public String sayGoodBye() {
+        return "";
+    }
+}
--- demoAssignment/src/test/java/nl/rug/oop/demoAssignment/MainTest.java
+++
@@ -1,9 +0,0 @@
-package nl.rug.oop.demoAssignment;
-
-/**
- * Tests the functionality of Main.java
- */
-public class MainTest {
-
-        
-}
