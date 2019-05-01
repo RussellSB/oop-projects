149b4461f281180f2c03197087f1be3df6b46852
RussellSB <r.sammutbonnici@gmail.com>
2019-05-01 16:34:32 +0200

    updated text

--- rpg/src/main/java/nl/rug/oop/introduction/HeavyDoor.java
+++ rpg/src/main/java/nl/rug/oop/introduction/HeavyDoor.java
@@ -27,11 +27,11 @@ public class HeavyDoor extends Door implements HealthModifier {
     public void interact(Player player) {
         increaseNpushes();
         if(getN_pushes() >= MIN_PUSHES) {
-            System.out.println("You pull with all your might. You slip through, slightly taking damage as it closes back behind you");
+            System.out.println("You pull with all your might. You slip through, slightly taking damage as the door closes behind you on your foot.\nOuch\nYou pull your foot out, aggrevated.");
             affectHealth(player, -10);
             super.interact(player);
         } else {
-            System.out.println("You pull with all your might. It opens by an inch");
+            System.out.println("You pull with all your might. It opens by an inch but your arms start aching. You're fussy and lose a little motivation to live\nYou could keep trying though.");
             affectHealth(player, -5);
         }
     }
