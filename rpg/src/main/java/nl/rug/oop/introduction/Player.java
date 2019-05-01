e74f4adddfa039bf205c4c530d10eabe0925be8e
RussellSB <r.sammutbonnici@gmail.com>
2019-05-01 14:35:21 +0200

    Added HP and unknown attack

--- rpg/src/main/java/nl/rug/oop/introduction/Player.java
+++ rpg/src/main/java/nl/rug/oop/introduction/Player.java
@@ -1,8 +1,12 @@
 package nl.rug.oop.introduction;
 
 public class Player {
+
+public static final int MAX_HEALTH = 100;
+
     // Attributes
     private Room currentRoom;
+    private int health=MAX_HEALTH;
 
     // Constructor
     public Player(Room currentRoom) {
@@ -18,6 +22,26 @@ public class Player {
         this.currentRoom = currentRoom;
     }
 
+    public int getHealth(){
+        return this.health;
+    }
+
+    public void affectHealth(int points){
+        if(this.health+points >= 100){
+            this.health = MAX_HEALTH;
+            System.out.println("Health maxed out! You're full of life and love.");
+        }else{
+            this.health = this.health+points;
+        }
+        
+    }
 
     // Other methods
+    public boolean stillAlive(){ 
+        if(this.health<0){
+            return false;
+        }else{
+            return true;
+        }
+    }
 }
--- rpg/src/main/java/nl/rug/oop/introduction/Unknown.java
+++ rpg/src/main/java/nl/rug/oop/introduction/Unknown.java
@@ -10,11 +10,13 @@ public class Unknown extends NPC{
     public void interact(Player player) {
         if(this.getMetBefore()==true){
             System.out.println("You fear the unknown but you remember that it also fears you too. You hesitate whether you should interact with it yet another time. You're careless though, you do it anyways.");
+            System.out.println("You feel a burning sensation in your leg. You hear a scream. You feel your leg melting. You arms detach. It was more than just a flesh wound.");
+            player.affectHealth(-100);
         }else{
             System.out.println("Though this entitity's breathing frightens you, a philosophical thought concerns you. What if this creature is just as scared of you as you are of it?");
             this.setMetBeforeTrue();
+            System.out.println(this.getResponse());
         }
-        System.out.println(this.getResponse());
     }
 
 }
