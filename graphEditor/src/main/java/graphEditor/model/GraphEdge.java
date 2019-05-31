dd2a8c416a6609956f9856aad2c28c236d831fb2
Andrés Salinas <i52salia@uco.es>
2019-06-01 01:51:14 +0200

    Progress done to the bulletproof Graph class.

--- graphEditor/src/main/java/graphEditor/model/GraphEdge.java
+++ graphEditor/src/main/java/graphEditor/model/GraphEdge.java
@@ -7,7 +7,17 @@ public class GraphEdge {
     private GraphVertex v1;
     private GraphVertex v2;
 
-    public GraphEdge(GraphVertex v1, GraphVertex v2) {
+    /**
+     *
+     * @param v1
+     * @param v2
+     * @throws IllegalArgumentException if v1 and v2 are the same
+     */
+    public GraphEdge(GraphVertex v1, GraphVertex v2) throws IllegalArgumentException {
+        // Self-loops are not allowed
+        if (v1 == v2)
+            throw new IllegalArgumentException("Self-loops are not allowed: v1 and v2 must be different");
+
         this.v1 = v1;
         this.v2 = v2;
     }
@@ -20,6 +30,11 @@ public class GraphEdge {
         return v2;
     }
 
+    public boolean hasVertex(GraphVertex v) {
+        return this.v1 == v || this.v2 == v;
+    }
+
+    @Override
     public String toString() {
         return this.v1.getName() + " " + this.v2.getName();
     }
